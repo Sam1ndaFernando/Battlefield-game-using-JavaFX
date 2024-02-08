828a161b4f2ad2158467be1220b58e78a6c7c408
Sam1ndaFernando <uthpala2001@gmail.com>
2024-02-08 10:22:40 +0530

    navigation foam created

---
+++ src/main/java/org/battlefieldGame/controller/NavigationController.java
@@ -0,0 +1,47 @@
+package org.battlefieldGame.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class NavigationController {
+
+    @FXML
+    private AnchorPane startWindow;
+
+    @FXML
+    private Label labelTitle;
+
+    @FXML
+    private AnchorPane anchorMainA;
+
+    @FXML
+    private JFXButton selectA;
+
+    @FXML
+    private AnchorPane anchorMainB;
+
+    @FXML
+    private JFXButton selectB;
+
+    @FXML
+    private JFXButton BattleStart;
+
+    @FXML
+    void BattleStartOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SelectBOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectAOnAction(ActionEvent event) {
+
+    }
+
+}
