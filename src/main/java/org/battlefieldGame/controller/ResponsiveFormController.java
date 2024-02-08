1359e1f4744469f8591b63da42d8056b90de7c10
Sam1ndaFernando <uthpala2001@gmail.com>
2024-02-08 20:48:41 +0530

    responsive form created

---
+++ src/main/java/org/battlefieldGame/controller/ResponsiveFormController.java
@@ -0,0 +1,21 @@
+package org.battlefieldGame.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class ResponsiveFormController {
+
+    @FXML
+    private AnchorPane startWindow;
+
+    @FXML
+    private JFXButton start;
+
+    @FXML
+    void startOnAction(ActionEvent event) {
+
+    }
+
+}
